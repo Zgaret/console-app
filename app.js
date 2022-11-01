700e80bc170388632d656c287f7ccb9082c610d8
Alex <zgaret@bk.ru>
2022-11-01 20:26:45 +0300

    improved the app by adding loops and conditions

--- app.js
+++ app.js
@@ -9,12 +9,55 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-let a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько вы его оцените?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько вы его оцените?', '');
+// Проверяем насколько пользователь любит кино и сообщаем ему об этом
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
+// с помощью цикла задаем вопросы и проверяем на правильность ответы
+// И записываем полученные ответы в объект
+// с циклом for
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько вы его оцените?', '');
+    if (a != 0 && b != 0 && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
+// let i = 0;
+// с циклом do while
+// do {
+//     let a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько вы его оцените?', '');
+//     if (a != 0 && b != 0 && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         i--;
+//     }
+//     i++;
+// } while (i < 2);
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+// c помощью цикла while
+// while (i < 2) {
+//     let a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько вы его оцените?', '');
+//     if (a != 0 && b != 0 && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         i--;
+//     }
+//     i++;
+// }
+
+
+
 
 console.log(personalMovieDB);
